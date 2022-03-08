ecef6f30492f27f0e7fb84d3e631580620b32ef0
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-08 14:09:36 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TechShreyash/SiestaXMusic",
+    "https://github.com/AMANTYA1/DURATE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
@@ -65,10 +65,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TechZBots"
+    "SUPPORT_CHANNEL", "https://t.me/The_Godfather_Network"
 )  # Example:- https://t.me/TheYukki
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TechZBots_Support"
+    "SUPPORT_GROUP", "https://t.me/GodfatherSupport"
 )  # Example:- https://t.me/YukkiSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
